71a191d33541b2da9e56d43c11060c50b3e6b9c3
pedram.enayati.kashani <pedram.en1376@gmail.com>
2024-09-20 16:20:58 +0330

    start project

---
+++ src/components/App.js
@@ -0,0 +1,61 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+      <div className="App container">
+          <h1 className="mb-3">cars</h1>
+          <div className="cars d-flex gap-3 flex-wrap">
+              <div className="card">
+                  <img src="/images/1.jpg" className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                      <p className="card-text">Some quick example text to build on the card title and make up the bulk
+                          of the
+                          card's content.</p>
+                  </div>
+              </div>
+              <div className="card">
+                  <img src="/images/1.jpg" className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                      <p className="card-text">Some quick example text to build on the card title and make up the bulk
+                          of the
+                          card's content.</p>
+                  </div>
+              </div>
+              <div className="card">
+                  <img src="/images/1.jpg" className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                      <p className="card-text">Some quick example text to build on the card title and make up the bulk
+                          of the
+                          card's content.</p>
+                  </div>
+              </div>
+              <div className="card">
+                  <img src="/images/1.jpg" className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                      <p className="card-text">Some quick example text to build on the card title and make up the bulk
+                          of the
+                          card's content.</p>
+                  </div>
+              </div>
+              <div className="card">
+                  <img src="/images/1.jpg" className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                      <p className="card-text">Some quick example text to build on the card title and make up the bulk
+                          of the
+                          card's content.</p>
+                  </div>
+              </div>
+              <div className="card">
+                  <img src="/images/1.jpg" className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                      <p className="card-text">Some quick example text to build on the card title and make up the bulk
+                          of the
+                          card's content.</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <header id="header" className="mb-3">
+            <div className="container px-0 py-2 d-flex justify-content-between align-items-center">
+                <img src="/logo192.png" alt="logo" width="80px"/>
+                <div className="titleHeader">New Project</div>
+                <div></div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
